58676f9bce7c3f620b031009709f90ccf30cdd60
nicole <mac@nicole-2.local>
2024-07-01 18:52:29 -0500

    fix

--- explore_forthright.R
+++ explore_forthright.R
@@ -1,47 +1,67 @@
 #This will join people's ideology and domain label from Forthright
 #Load library + import data
 library(tidyverse)
-library(dplyr)
 ideology <- load("data/forthright_ideology.RData")
 classify <- load("data/classify_forthright.RData")
 
 
 #Join dataset
-forthright_ideology <- forthright_ideology %>% 
+forthright_join <- forthright_ideology %>% 
   select(member_id, Q13, slant) %>%
   distinct() 
 
 explore_forthright <- forthright_label %>% 
-  left_join(forthright_ideology, by="member_id")
+  left_join(forthright_join, by="member_id")
 
-#Explore dataset----------
-#There are 410 people visited untrustworthy sites out of 584 (70.2%)
-explore_forthright %>% 
-  group_by(member_id) %>%
-  distinct() %>% 
-  count ()
+##Explore dataset----------
+#Observations = 584
+forthright_ideology %>% 
+  summarise(unique_ids = n_distinct(member_id))
 
-#Distribution left/right wing
-#Left wing = 146 (35.6%)
+#People who visited untrustworthy sites = 410
 explore_forthright %>% 
-  group_by(member_id, slant) %>%
-  filter(slant=="left") %>% 
-  distinct(member_id) %>% 
-  tally ()
-#Right wing = 170 (41.46%)
-explore_forthright %>% 
-  group_by(member_id, slant) %>%
-  filter(slant=="right") %>% 
-  distinct(member_id) %>% 
-  tally ()
-#Neutral = 93 (22.68%)
-explore_forthright %>% 
-  group_by(member_id, slant) %>%
-  filter(slant=="neutral") %>% 
-  distinct(member_id) %>% 
-  tally ()
+  summarise(unique_ids = n_distinct(member_id))
 
-#Finding this weird guy--political side is NA, member_id=4433402
+410/584*100 #70.2% of the observations visit these sites
+
+#Political affiliation
+#Total left = 207 (35.4% of 584)
+forthright_ideology %>% 
+  filter(slant=='left') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+#Left visits = 146 (35.6% of 410)
+explore_forthright %>% 
+  filter(slant=='left') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+146/207*100 #70.5% of people from left wing visit untrustworthy sites
+
+#Total right = 231 (39.55% of 584)
+forthright_ideology %>% 
+  filter(slant=='right') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+#Right visits = 170 (41.46% of 410)
+explore_forthright %>% 
+  filter(slant=='right') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+170/231*100 #73.59% of people from right wing visit untrustworthy sites
+
+#Total neutral = 144 (24.7% of 584)
+forthright_ideology %>% 
+  filter(slant=='neutral') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+#Neutral visits = 93 (22.68% of 410)
+explore_forthright %>% 
+  filter(slant=='neutral') %>% 
+  summarise(unique_ids = n_distinct(member_id))
+
+93/144*100 #64.58% of people who are neutral visit untrustworthy sites
+
+#Finding this weird guy--political is NA, member_id=4433402
 explore_forthright %>% 
   group_by(member_id, slant) %>%
   filter(!slant %in% c("left","right","neutral")) %>% 
@@ -52,3 +72,5 @@ explore_forthright %>%
   filter (member_id==4433402, label=="fake") %>% 
   group_by(domain) %>% 
   tally()
+
+
